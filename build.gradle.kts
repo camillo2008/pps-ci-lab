b98e05d77218a00b284c0ab1eca8a8d30dde306a
Ruben Ceroni <ruben.ceroni@studio.unibo.it>
2020-05-04 17:40:48 +0200

    generate jacoco reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     java
     scala
+    jacoco
 }
+
+
+jacoco {
+    toolVersion = "0.8.5"
+    reportsDir = file("$buildDir/customJacocoReportDir")
+}
+
+
 repositories {
     mavenCentral()
 }
@@ -10,6 +19,18 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
 }
+
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+}
+
+
+
 tasks.named<Test>("test"){
     useJUnitPlatform()
 }
