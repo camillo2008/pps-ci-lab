eb1c053f8bb65c2320d400013912efa3d7385a62
Gyordan <gyordan.caminati@studio.unibo.it>
2020-05-04 16:56:10 +0200

    Modifica build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,12 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.1")
+	implementation("org.scala-lang:scala-library:+")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:+")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:+")
+}
+
+// Enables JUnit Platform (needed for JUnit 5)
+tasks.named<Test>("test") {
+	useJUnitPlatform()
 }
